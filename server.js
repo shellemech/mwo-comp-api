19fc76d99eec9049a3a2d65723b24e98d7ef2809
Ubuntu <ubuntu@ip-172-31-4-187.us-west-2.compute.internal>
2018-01-16 00:21:03 +0000

    added nav page skels

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express')
   , logger = require('morgan')
   , app = express()
   , template = require('jade').compileFile(__dirname + '/source/templates/homepage.jade')
+  , basedir = 'source/templates/'
 
 app.use(logger('dev'))
 app.use(express.static(__dirname + '/static'))
@@ -15,6 +16,18 @@ app.get('/', function (req, res, next) {
   }
 })
 
+app.get('/submit', function (req, res) {
+    var html = require('jade').renderFile(basedir + 'submit.jade')
+    res.send(html)
+});
+app.get('/contact', function (req, res) {
+    var html = require('jade').renderFile(basedir + 'contact.jade')
+    res.send(html)
+});
+app.get('/about', function (req, res) {
+    var html = require('jade').renderFile(basedir + 'about.jade')
+    res.send(html)
+});
 app.listen(process.env.PORT || 3000, function () {
   console.log('Listening on http://localhost:' + (process.env.PORT || 3000))
 })
