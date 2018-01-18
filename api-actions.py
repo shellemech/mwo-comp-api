8f3762332380d4358713d67d7978efa1870a13e6
shellemech <shellemech@gmail.com>
2018-01-18 00:53:25 +0000

    added python funcs

---
+++ api-actions.py
@@ -0,0 +1,49 @@
+import urllib.request
+import urllib.error
+import requests
+import sys
+import os
+
+def main():
+  apikey = os.environ['apikey']
+  mwourl = "https://mwomercs.com/api/v1/matches/{}?api_token={}".format(matchid, apikey)
+  getmatchdata(mwourl)
+
+# Get matchdata from MWO
+def getmatchdata(url):
+  try: 
+    request = urllib.request.Request(url)
+    result = urllib.request.urlopen(request)
+  except urllib.error.HTTPError as e:
+    print('MWO API Error: '+e.reason+'. HTTP Error code', e.code)
+    sys.exit(1)
+  except urllib.error.URLError as e:
+    print('MWO API URL Error:'+e.reason)
+    sys.exit(1)
+  else:
+    matchjson = str(result.read(), 'utf-8')
+    postmatchdata(matchjson)
+
+# Format and post to Elasticsearch
+def postmatchdata(json):
+  esobj = '{"index": {"_index": "mwo", "_type": "match", "_id": "'+ matchid +'"}}\n'
+  headers = {'Content-Type': 'application/x-ndjson'}
+  json+='\n'
+  esobj+=json
+  try:
+    espost = requests.post('http://localhost:9200/_bulk?pretty', headers=headers, data=esobj)
+  except requests.exceptions.RequestException as e:
+    print('Elasticsearch POST Error: ', e)
+    sys.exit(1) 
+  else:
+    output = espost.json()
+    try:
+      print(output['status'])
+    except KeyError:
+      print('Success! Match '+ matchid +' has been added to the indexer.')
+    else:
+      print('Error inserting match '+ matchid +' in to index, check Elasticsearch logs :(')
+
+if __name__ == '__main__':
+  matchid = sys.argv[1]
+  main()
--- server.js
+++ server.js
@@ -10,15 +10,33 @@ app.set('views', './source/templates')
 app.get('/', function(req, res) {
   res.render('homepage')
 })
-app.get('/submit', function (req, res) {
-  res.render('submit')
-});
 app.get('/contact', function (req, res) {
   res.render('contact')
-});
-app.get('/about', function (req, res) {
-  res.render('about')
-});
+})
+app.get('/submit', function (req, res) {
+  res.render('submit')
+})
+
+// parameter middleware that will run before the next routes
+app.param('matchid', function(req, res, next, matchid) {
+
+  // run python script to get match data and upload to elasticsearch
+  var output
+
+  const { exec } = require('child_process')
+  exec('python3 api-actions.py "'+ matchid +'"', (err, stdout, stderr) => {
+    if (err) output = err
+    else output = stdout
+    console.log(output)
+    res.locals.matchid = matchid
+    res.locals.output = output
+    next()
+  })
+})
+
+app.get('/match/:matchid', function(req, res) {
+    res.render('match', { matchid: res.locals.matchid, output: res.locals.output })
+})
 
 app.listen(process.env.PORT || 3000, function () {
   console.log('Listening on http://localhost:' + (process.env.PORT || 3000))
